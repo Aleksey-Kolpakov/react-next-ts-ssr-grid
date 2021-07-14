f4f612becf308bdb2578b6ff00fe4e3c40051d72
Aleksey-Kolpakov <gmsaleksei@gmail.com>
2021-07-14 20:16:05 +0300

    add layout and footer css

--- next-ts-app/layout/Footer/Footer.tsx
+++ next-ts-app/layout/Footer/Footer.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
+import {format} from 'date-fns';
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+
+const Footer = ({ className,...props }: FooterProps): JSX.Element => {
+  return <div className={cn(styles.footer,className)} {...props}>
+    <p className={styles.rights}>OwlTop © 2020 - {format(new Date(),'yyyy')} Все права защищены</p>
+    <a href="#" target="_blank" className={styles.agreement}>Пользовательское соглашение</a>
+    <a href="#" target="_blank" className={styles.policy}>Политика конфиденциальности</a>
+    </div>;
 };
 
 export default Footer;
--- next-ts-app/layout/Layout.tsx
+++ next-ts-app/layout/Layout.tsx
@@ -8,25 +8,25 @@ import Footer from "./Footer/Footer";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
 export default Layout;
 
-export const withLayout=<T extends Record<string,unknown>>(Component:FunctionComponent<T>)=>{
-return function ComponentWithLayout(props:T):JSX.Element {
-  return (
-    <Layout>
-      <Component {...props}/>
-    </Layout>
-  );
-};
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
+  return function ComponentWithLayout(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
 };
