463b2c19c5ff482ade427d8f7b4b6e7a3fd60ecf
Aleksey-Kolpakov <gmsaleksei@gmail.com>
2021-08-10 19:02:27 +0300

    add routes and helpers

---
+++ next-ts-app/helpers/helpers.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FirstLevelMenuItem } from "../interfaces/menu.interface";
+import { TopLevelCategory } from "../interfaces/page.interface";
+import CoursesIcon from "./icons/cources.svg";
+import BooksIcon from "./icons/books.svg";
+import ProductsIcon from "./icons/products.svg";
+import ServicesIcon from "./icons/services.svg";
+
+export const firstLevelMenu: FirstLevelMenuItem[] = [
+    {
+      route: "courses",
+      name: "Курсы",
+      icon: <CoursesIcon />,
+      id: TopLevelCategory.Courses,
+    },
+    {
+      route: "books",
+      name: "Книги",
+      icon: <BooksIcon />,
+      id: TopLevelCategory.Books,
+    },
+    {
+      route: "products",
+      name: "Продукты",
+      icon: <ProductsIcon />,
+      id: TopLevelCategory.Products,
+    },
+    {
+      route: "services",
+      name: "Сервисы",
+      icon: <ServicesIcon />,
+      id: TopLevelCategory.Services,
+    },
+  ];
--- next-ts-app/layout/Menu/Menu.tsx
+++ next-ts-app/layout/Menu/Menu.tsx
@@ -3,42 +3,9 @@ import styles from "./Menu.module.css";
 import { FirstLevelMenuItem, MenuItem, PageItem } from "../../interfaces/menu.interface";
 import cn from "classnames";
 import { AppContext } from "../../context/app.context";
-import CoursesIcon from "./icons/cources.svg";
-import BooksIcon from "./icons/books.svg";
-import ProductsIcon from "./icons/products.svg";
-import ServicesIcon from "./icons/services.svg";
-import { TopLevelCategory } from "../../interfaces/page.interface";
 import Link from 'next/link';
 import { useRouter } from "next/dist/client/router";
-
-
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  {
-    route: "courses",
-    name: "Курсы",
-    icon: <CoursesIcon />,
-    id: TopLevelCategory.Courses,
-  },
-  {
-    route: "books",
-    name: "Книги",
-    icon: <BooksIcon />,
-    id: TopLevelCategory.Books,
-  },
-  {
-    route: "products",
-    name: "Продукты",
-    icon: <ProductsIcon />,
-    id: TopLevelCategory.Products,
-  },
-  {
-    route: "services",
-    name: "Сервисы",
-    icon: <ServicesIcon />,
-    id: TopLevelCategory.Services,
-  },
-];
+import {firstLevelMenu} from '../../helpers/helpers';
 
 const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
